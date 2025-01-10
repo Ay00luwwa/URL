8d56ad7e731d4d1e428a6e74668041f195179277
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2025-01-11 00:40:05 +0100

    static changes

--- backend/shortner/settings.py
+++ backend/shortner/settings.py
@@ -126,7 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'  # This should already be present
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Set this to your desired path
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
